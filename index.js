2909413725ec713deac08e7829f77d0d008f1f5f
Ruvi <ruvindihulangamuwa@gmail.com>
2023-06-11 11:14:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function cookLunch(a) {
+  let cookingMainItems = [];
+  console.log("Get things out of fridge");
+  cookingMainItems.push("Chicken");
+  cookingMainItems.push("Beans");
+  cookingMainItems.push("Potatoes");
+  console.log("Heat pan");
+  for (let i = 0; i < cookingMainItems.length; i++) {
+    cutThingsWithKnife(cookingMainItems[i]);
+  }
+  console.log("Add ingridients");
+  console.log("Stir and cook");
+  console.log("Serve");
+}
